5a154869562d86af314af841aa28f071cf33b100
tenko08 <unalai1027@gmail.com>
2025-11-27 17:16:48 -0500

    done

--- src/main/java/regex/Main.java
+++ src/main/java/regex/Main.java
@@ -38,7 +38,11 @@ public class Main {
      * @return whether the string satisfies the password requirements
      */
     public static boolean checkForPassword(String str, int minLength) {
-        final boolean propertyOne = Pattern.matches("REPLACE WITH CORRECT REGEX", str);
+        if (str == null) {
+            return false;
+        }
+        final String regex = String.format("(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{%d,}", minLength);
+        final boolean propertyOne = Pattern.matches(regex, str);
         // as needed, modify this code.
         return propertyOne;
     }
@@ -55,9 +59,12 @@ public class Main {
      * @return a list containing the email addresses in the string.
      */
     public static List<String> extractEmails(String str) {
-        final Pattern pattern = Pattern.compile("REPLACE WITH CORRECT REGEX");
-        final Matcher matcher = pattern.matcher(str);
         final List<String> result = new ArrayList<>();
+        if (str == null) {
+            return result;
+        }
+        final Pattern pattern = Pattern.compile("[\\w.%+-]+@(mail\\.)?utoronto\\.ca");
+        final Matcher matcher = pattern.matcher(str);
         while (matcher.find()) {
             result.add(matcher.group());
         }
@@ -76,6 +83,9 @@ public class Main {
      * @return whether str contains the same capital letter twice.
      */
     public static boolean checkForDoubles(String str) {
-        return str.matches("replace with correct regex");
+        if (str == null) {
+            return false;
+        }
+        return str.matches(".*([A-Z]).*\\1.*");
     }
 }
